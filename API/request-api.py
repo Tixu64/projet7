b120f7db1820fed31213dc3d2bd61a3b08937728
Tixu64 <murielraynaud@free.fr>
2021-02-04 14:48:10 +0100

    Mise à jour Livrables

--- API/request-api.py
+++ API/request-api.py
@@ -12,12 +12,15 @@ with open('df_client_dash.pkl','rb') as fp:
 with open('select_features.pkl','rb') as fp:
     select_features= pickle.load(fp)
 
-
+print('ID cient :',381675)
 data = data = df_client[select_features].loc[df_client.SK_ID_CURR == 381675]
+print('Données du client')
+print(data)
 #converting it into dictionary
 data=data.to_dict('records')
 #converting json
 data_json = json.dumps(data)
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 r = requests.post(url, data=data_json, headers=headers)
+print("Résultat de la prédiction")
 print(r, r.text)
